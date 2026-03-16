96758c665db87ec2af5f837076cc705072dd7f90
J03L5009 <joelfernando5009@gmail.com>
2026-03-16 15:33:05 -0400

    Fracciones de Joel

--- aritmetica.py
+++ aritmetica.py
@@ -2,3 +2,66 @@ def multiplicacion_ticonaplata_jofre(a, b):
     return a * b
 
 print("Resultado:", multiplicacion_ticonaplata_jofre(6, 7))
+
+
+
+
+def fraccion(a, b):
+    return str(a) + "/" + str(b)
+
+print("Fracción (3/4):", fraccion(3, 4))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+x
+^G Help       ^O Write Out  ^F Where Is   ^K Cut        ^T Execute    ^C Location   M-U Undo
+^X Exit       ^R Read File  ^\ Replace    ^U Paste      ^J Justify    ^/ Go To Line M-E Redo
