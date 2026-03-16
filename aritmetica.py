311b15520433937b88df85998051e608704ee3e6
JoseAL <202203245@est.umss.edu>
2026-03-16 15:45:08 -0400

    Agregar funcion RESTA de Aima Lupa  Jose

--- aritmetica.py
+++ aritmetica.py
@@ -2,3 +2,8 @@ def multiplicacion_ticonaplata_jofre(a, b):
     return a * b
 
 print("Resultado:", multiplicacion_ticonaplata_jofre(6, 7))
+
+def resta_aimalupa_jose(a, b):
+    return a - b
+
+print("Resultado:", resta_aimalupa_jose(10, 5))
