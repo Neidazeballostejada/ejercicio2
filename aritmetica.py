47700f4dde1a77a1253b4b427cf9c296f61655ba
WILBER LANCEA MAMANI <202202533@est.umss.edu>
2026-03-16 11:21:50 -0400

    Funcion division Wilber

--- aritmetica.py
+++ aritmetica.py
@@ -2,3 +2,8 @@ def multiplicacion_ticonaplata_jofre(a, b):
     return a * b
 
 print("Resultado:", multiplicacion_ticonaplata_jofre(6, 7))
+
+def division_wilberLancea(a, b):
+    return a / b
+
+print("Resultado:", division_wilberLancea(10, 2))
