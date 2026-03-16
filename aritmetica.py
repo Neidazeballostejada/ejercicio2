1e1f86b07d5f6bbb2900aeed3e4fe53b869e1ba1
Madahi <condoriperezmadahikaren@gmail.com>
2026-03-16 16:09:41 -0400

    Agregar funcion Radicacion de Madahi karen Condori Perez

--- aritmetica.py
+++ aritmetica.py
@@ -2,3 +2,9 @@ def multiplicacion_ticonaplata_jofre(a, b):
     return a * b
 
 print("Resultado:", multiplicacion_ticonaplata_jofre(6, 7))
+
+def radicacion(numero, indice):
+    return numero ** (1/indice)
+
+print("Resultado:", radicacion(27, 3))
+
