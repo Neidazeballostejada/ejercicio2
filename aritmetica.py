856994b04f2f39e1b66b7fd733e61a1383c6530f
WILBER LANCEA MAMANI <202202533@est.umss.edu>
2026-03-16 20:42:03 -0400

    Agregar función división de Wilber

--- aritmetica.py
+++ aritmetica.py
@@ -3,7 +3,7 @@ def multiplicacion_ticonaplata_jofre(a, b):
 
 print("Resultado:", multiplicacion_ticonaplata_jofre(6, 7))
 
-def division_wilberLancea(a, b):
+def division_wilber(a, b):
     return a / b
 
-print("Resultado:", division_wilberLancea(10, 2))
+print("Resultado:", division_wilber(10, 2))
